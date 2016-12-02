0a379d4dc1041f1374d1a1eb3a11a75cf31692b7
Steven Frazier-Roberts <steven@Stevens-MacBook-Pro.local>
2016-12-02 14:14:07 +0000

    new

---
+++ src/main/sencha/build/temp/development/myApp/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../myapp/sass/config.rb')
+cache_path = '/Users/steven/Downloads/work/extjsmaven/src/main/sencha/build/.sass-cache'
---
+++ src/main/sencha/build/temp/development/myApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/steven/Downloads/work/extjsmaven/src/main/sencha/myapp/app.js
